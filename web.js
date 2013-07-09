79c02e3670cc0956f5112f10dc47ac6a5136436d
Laszlo Velinszky <djlancelot@freemail.hu>
2013-07-09 10:46:20 +0200

    Bugfix in web.js

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  contents = fs.readFileSync('index.html');
+  buffer = fs.readFileSync('index.html');
   response.send(buffer.toString('utf-8'));
 });
 
